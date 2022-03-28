c5fae8b1ea0656debc16437674ff1891be6c5efc
yassine <medyassine.essamadi@ynov.com>
2022-03-28 19:19:13 +0200

    scrap 1000 pages

--- scrap.py
+++ scrap.py
@@ -12,43 +12,34 @@ data = {
 
 
 headers = {'User-Agent': 'Mozilla/5.0'}
-url = 'https://www.metacritic.com/search/game/results'
 
 
-html = rq.get(url,headers=headers)
-soup = BeautifulSoup(html.content,'html.parser')
 
-# store  html file 
-with open('test.html','w', encoding="utf-8") as file :
-    file.write(html.text)
+"""html = rq.get(url,headers=headers)
+soup = BeautifulSoup(html.content,'html.parser')"""
+
+
 
 # get flatforms 
-platforms = soup.findAll('span' , {'class' : 'platform'})
-list_paltforms = [x.text for x in platforms]
-data['Platform'] = list_paltforms
+for p in range(0,100) :
+    
+    url = f'https://www.metacritic.com/search/game/results?page={p}'
+    html = rq.get(url,headers=headers)
+    soup = BeautifulSoup(html.content,'html.parser')
+    platforms = soup.findAll('span' , {'class' : 'platform'})
+    list_paltforms = [x.text for x in platforms]
+    data['Platform'].extend(list_paltforms)
+    meta_scores = soup.findAll('span' , {'class' : 'metascore_w'})
+    list_scores = [x.text for x in meta_scores]
+    data['Review'].extend(list_scores)
+    meta_links = soup.findAll('h3',{'class':'product_title basic_stat'})
+    list_titles = [x.text.strip() for x in meta_links]
+    data['Name'].extend(list_titles)
+    meta_years = soup.select('.main_stats p')
+    list_years = [i.find('span').next_sibling.text.strip() for i in meta_years]
+    data['Year'].extend(list_years)
+    
 
-# get review
-meta_scores = soup.findAll('span' , {'class' : 'metascore_w'})
-list_scores = [x.text for x in meta_scores]
-data['Review'] = list_scores
-
-
-# get links
-
-meta_links = soup.findAll('h3',{'class':'product_title basic_stat'})
-list_titles = [x.text.strip() for x in meta_links]
-data['Name'] = list_titles
-
-
-meta_years = soup.select('.main_stats p')
-
-list_years = [i.find('span').next_sibling.text.strip() for i in meta_years]
-
-data['Year'] = list_years
 
 df = pd.DataFrame(data)
-df.to_csv('video_games_review.csv')
-# list_years = [i.text.strip() for i in meta_years]
-
-# list_years = [x.text.strip() for x in meta_years]
-
+df.to_csv('./data/video_games_reviews.csv')
