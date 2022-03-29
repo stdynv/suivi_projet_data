d563c177cfb859aebcb3d83a42c1f3ae25672dd7
yassine <medyassine.essamadi@ynov.com>
2022-03-29 09:35:25 +0200

    scrap & update dataset

--- scrap.py
+++ scrap.py
@@ -21,24 +21,27 @@ soup = BeautifulSoup(html.content,'html.parser')"""
 
 
 # get flatforms 
-for p in range(0,100) :
-    
-    url = f'https://www.metacritic.com/search/game/results?page={p}'
+for p in range(0,193) :
+    print(p)
+    url = f'https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?page={p}'
     html = rq.get(url,headers=headers)
     soup = BeautifulSoup(html.content,'html.parser')
-    platforms = soup.findAll('span' , {'class' : 'platform'})
-    list_paltforms = [x.text for x in platforms]
+
+    meta_links = soup.select('a.title > h3')
+    list_titles = [x.text for x in meta_links]
+    data['Name'].extend(list_titles)
+
+    platforms = soup.find('div',class_='next_to_side_col').findAll('span',class_='data')
+    list_paltforms = [x.text.strip() for x in platforms]
     data['Platform'].extend(list_paltforms)
-    meta_scores = soup.findAll('span' , {'class' : 'metascore_w'})
+
+    meta_scores = soup.select('.clamp-score-wrap .metascore_w')
     list_scores = [x.text for x in meta_scores]
     data['Review'].extend(list_scores)
-    meta_links = soup.findAll('h3',{'class':'product_title basic_stat'})
-    list_titles = [x.text.strip() for x in meta_links]
-    data['Name'].extend(list_titles)
-    meta_years = soup.select('.main_stats p')
-    list_years = [i.find('span').next_sibling.text.strip() for i in meta_years]
+
+    meta_years = soup.select('.clamp-details > span')
+    list_years = [i.text for i in meta_years]
     data['Year'].extend(list_years)
-    
 
 
 df = pd.DataFrame(data)
